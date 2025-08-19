8c1c3681b998d7c99809bc5a292275ee0adf9632
bloopotato <133745515+bloopotato@users.noreply.github.com>
2025-08-19 23:35:06 +0800

    add screens for adding off device

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     'Poppins-Bold': require('@/assets/fonts/Poppins-Bold.ttf'),
-    'Poppins-Medium': require('@/assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('@/assets/fonts/Poppins-SemiBold.ttf'),
     'Poppins-Regular': require('@/assets/fonts/Poppins-Regular.ttf'),
   });
 
@@ -24,6 +24,7 @@ export default function RootLayout() {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(user)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
