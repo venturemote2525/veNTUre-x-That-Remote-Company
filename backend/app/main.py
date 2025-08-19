58fe647693bb4dbc1d538fef64a0f90c33842fa0
jovinanna <160099103+jovinann@users.noreply.github.com>
2025-08-19 17:12:17 +0800

    basic logina and sign up auth

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,13 @@
 # dummy data for now
 from fastapi import FastAPI
 from .db.supabase_client import supabase  
-from .routes import food
+from .routes import food, auth
 
 app = FastAPI()
 
 # Include routers
 app.include_router(food.router, prefix="/food", tags=["food"])
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
 
 @app.get("/")
 def root():
--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -2,6 +2,16 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 
+# --- Auth ---
+class SignupRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+# --- Food ---
 class FoodItem(BaseModel):
     name: str
     calories: int
