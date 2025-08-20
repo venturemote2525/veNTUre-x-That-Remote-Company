c9b1ebf76499f4c989ff435df26a8255829303e0
bloopotato <133745515+bloopotato@users.noreply.github.com>
2025-08-21 01:55:46 +0800

    add tab bar and tab screens

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,47 +1,58 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import TabBar from '@/components/TabBar';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {
+  faHome,
+  faUtensils,
+  faCamera,
+  faChild,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
+      tabBar={props => <TabBar {...props} />}
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}
-    >
+      }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+            <FontAwesomeIcon icon={faHome} size={24} color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="food"
         options={{
-          title: 'Explore',
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <FontAwesomeIcon icon={faUtensils} size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="logging"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faCamera} size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="body"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faChild} size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faUser} size={24} color={color} />
           ),
         }}
       />
