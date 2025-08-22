122863c7c0f839d09b023627f4bfa0470ee7d1e3
bloopotato <133745515+bloopotato@users.noreply.github.com>
2025-08-23 02:17:36 +0800

    add food logs screen

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -2,13 +2,19 @@ import CustomDropdown, { DropdownItem } from '@/components/CustomDropdown';
 import { ThemedSafeAreaView, Text, View } from '@/components/Themed';
 import { AddIcon, Icon } from '@/components/ui/icon';
 import { useRouter } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Pressable, useColorScheme } from 'react-native';
+import { faUtensils, faChild } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { Colors } from '@/constants/Colors';
 
 export default function HomeScreen() {
   const router = useRouter();
+  const rawScheme = useColorScheme();
+  const scheme: 'light' | 'dark' = rawScheme === 'dark' ? 'dark' : 'light';
   return (
-    <ThemedSafeAreaView className="p-4">
-      <View className="flex-row justify-between">
+    <ThemedSafeAreaView>
+      {/* Header */}
+      <View className="flex-row justify-between p-4">
         <Text className="font-heading text-head2 text-secondary-500">
           HealthSync
         </Text>
@@ -27,6 +33,56 @@ export default function HomeScreen() {
           />
           <DropdownItem label={'Manual Input'} />
         </CustomDropdown>
+      </View>
+      {/* Main */}
+      <View className="flex-1 gap-4 px-4">
+        {/* Food */}
+        <Pressable
+          onPress={() => router.push('/(tabs)/food')}
+          className="card-white flex-row justify-between">
+          {/* Title */}
+          <View className="flex-row items-center gap-4">
+            <View className="h-10 w-10 items-center justify-center rounded-full bg-secondary-500">
+              <FontAwesomeIcon
+                icon={faUtensils}
+                size={18}
+                color={Colors[scheme].background}
+              />
+            </View>
+            <Text className="font-bodyBold text-body1 text-primary-500">
+              Food
+            </Text>
+          </View>
+          <View className="items-center">
+            <Text className="font-heading text-body1 text-primary-500">10</Text>
+            <Text className="font-bodyBold text-body2 text-primary-500">
+              kcal
+            </Text>
+          </View>
+        </Pressable>
+        {/* Body */}
+        <Pressable
+          onPress={() => router.push('/(tabs)/body')}
+          className="card-white gap-4">
+          {/* Title */}
+          <View className="flex-row items-center gap-4">
+            <View className="h-10 w-10 items-center justify-center rounded-full bg-secondary-500">
+              <FontAwesomeIcon
+                icon={faChild}
+                size={18}
+                color={Colors[scheme].background}
+              />
+            </View>
+            <Text className="font-bodyBold text-body1 text-primary-500">
+              Body Composition
+            </Text>
+          </View>
+          {/* Details */}
+          <View>
+            <Text>Body Fat:</Text>
+            <Text>Weight:</Text>
+          </View>
+        </Pressable>
       </View>
     </ThemedSafeAreaView>
   );
