aba6fdace5245252828d46b154069c5eafb8a11a
bloopotato <yuhuineo@gmail.com>
2025-08-29 02:52:22 +0800

    added uploading of images to supabase

--- frontend/types/database-types.ts
+++ frontend/types/database-types.ts
@@ -11,3 +11,19 @@ export interface UserProfile {
 }
 
 export type UserProfileInsert = Omit<UserProfile, 'id' | 'created_at'>;
+
+// -------------------- meals --------------------
+
+export interface Meal {
+  id: string;
+  user_id: string;
+  name: string;
+  image_url: string;
+  meal: string;
+  calories: number;
+  protein: number;
+  carbs: number;
+  fat: number;
+  date: string;
+  created_at: string;
+}
