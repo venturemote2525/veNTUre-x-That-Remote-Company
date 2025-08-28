30b40a7336d0ac158e2eec8dc51c1910cf9c3c1d
bloopotato <yuhuineo@gmail.com>
2025-08-29 02:52:22 +0800

    added uploading of images to supabase

--- frontend/app/(tabs)/logging.tsx
+++ frontend/app/(tabs)/logging.tsx
@@ -4,12 +4,21 @@ import * as ImagePicker from 'expo-image-picker';
 import { Pressable, Image } from 'react-native';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { useAuth } from '@/context/AuthContext';
+import { uploadImage } from '@/utils/food/api';
+import LoadingScreen from '@/components/LoadingScreen';
+import uuid from 'react-native-uuid';
 
 export default function LoggingScreen() {
+  const { profile } = useAuth()
   const [image, setImage] = useState<string | null>(null);
   const router = useRouter();
   const { meal: paramMeal } = useLocalSearchParams();
   const [meal, setMeal] = useState(paramMeal ?? '');
+  const [loading, setLoading] = useState(false)
+  const [uploadLoading, setUploadLoading] = useState(false)
+  const [aiLoading, setAiLoading] = useState(false)
+  const id = uuid.v4()
 
   useEffect(() => {
     if (paramMeal) {
@@ -24,11 +33,12 @@ export default function LoggingScreen() {
       allowsEditing: true,
       aspect: [1, 1],
       quality: 1,
+      base64: true,
     });
     if (result.assets && result.assets.length > 0) {
       console.log('select photo');
-      const uri = result.assets[0].uri;
-      setImage(uri);
+      const base64Data = result.assets[0].base64!;
+      setImage(base64Data);
     } else {
       console.log('no image selected');
     }
@@ -39,30 +49,55 @@ export default function LoggingScreen() {
       allowsEditing: true,
       aspect: [1, 1],
       quality: 1,
+      base64: true,
     });
 
     if (result.assets && result.assets.length > 0) {
       console.log('take photo');
-      const uri = result.assets[0].uri;
-      setImage(uri);
+      const base64Data = result.assets[0].base64!;
+      setImage(base64Data);
     } else {
       console.log('no image taken');
     }
   };
 
-  const confirmPhoto = () => {
-    if (!image) return;
-    router.push({
-      pathname: '/(logging)/summary',
-      params: { image: image, meal: meal },
-    });
+  const confirmPhoto = async () => {
+    if (!image || !profile) return;
+    try {
+      setLoading(true)
+      // Upload to database
+      setUploadLoading(true)
+      const path = await uploadImage(id, profile.user_id, image);
+      setUploadLoading(false)
+
+      // TODO: Send to AI
+      setAiLoading(true)
+      setAiLoading(false)
+
+      setLoading(false)
+      // Go to summary screen
+      router.push({
+        pathname: '/(logging)/summary',
+        params: { mealId: id, meal: meal },
+      });
+    } catch (error) {
+      console.log('Upload meal error: ', error);
+    }
   };
+
+  if (loading) {
+    return (
+        <ThemedSafeAreaView>
+          <LoadingScreen text={uploadLoading ? "Uploading photo" : aiLoading ? "Analysing photo" : "Loading"} />
+        </ThemedSafeAreaView>
+    )
+  }
 
   return (
     <ThemedSafeAreaView edges={['top']} className="px-4 py-8">
       <View className="flex-1 items-center gap-8 pt-16">
         {image ? (
-          <Image source={{ uri: image }} className="h-80 w-80 rounded-3xl" />
+          <Image source={{ uri: `data:image/jpeg;base64,${image}` }} className="h-80 w-80 rounded-3xl" />
         ) : (
           <View className="h-80 w-80 items-center justify-center rounded-3xl border-2 border-secondary-500 bg-background-0">
             <Text className="text-secondary-500">
