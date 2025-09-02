2df489a7e3162c08e5121abad49e4caefb7eaf41
bloopotato <133745515+bloopotato@users.noreply.github.com>
2025-09-02 23:56:05 +0800

    cleanup

--- frontend/types/database-types.ts
+++ frontend/types/database-types.ts
@@ -15,17 +15,17 @@ export type UserProfileInsert = Omit<UserProfile, 'id' | 'created_at'>;
 // -------------------- meals --------------------
 
 export interface Meal {
-    id: string;
-    user_id: string;
-    name: string;
-    image_url: string;
-    meal: string;
-    calories: number;
-    protein: number;
-    carbs: number;
-    fat: number;
-    date: string;
-    created_at: string;
+  id: string;
+  user_id: string;
+  name: string;
+  image_url: string;
+  meal: string;
+  calories: number;
+  protein: number;
+  carbs: number;
+  fat: number;
+  date: string;
+  created_at: string;
 }
 
 // -------------------- devices --------------------
