b957e155bc40ba28dfec834f06ba53bae03b47c4
jovinanna <160099103+jovinann@users.noreply.github.com>
2025-09-22 22:13:08 +0800

    fixed the sign-up page view errors

--- frontend/app/(auth)/signup.tsx
+++ frontend/app/(auth)/signup.tsx
@@ -1,20 +1,14 @@
-import { ThemedSafeAreaView, Text, TextInput, View } from '@/components/Themed';
 import PasswordInput from '@/components/Auth/PasswordInput';
+import { ThemedSafeAreaView, Text, View, TextInput } from '@/components/Themed';
 import { useRouter } from 'expo-router';
 import { useState } from 'react';
+import { AnimatedPressable } from '@/components/AnimatedComponents';
 import { checkPasswordStrength, checkValidEmail } from '@/utils/auth/auth';
 import { checkUserExists, userSignup } from '@/utils/auth/api';
-import { CustomAlert } from '@/components/CustomAlert';
-import { AnimatedPressable, useFadeIn } from '@/components/AnimatedComponents';
 
 export default function SignUp() {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState({
-    visible: false,
-    title: '',
-    message: '',
-  });
   const [fields, setFields] = useState({
     email: '',
     password: '',
@@ -24,14 +18,14 @@ export default function SignUp() {
     email: '',
     password: '',
     reenter: '',
-    login: '',
+    signup: '',
   });
 
-  const fadeIn = useFadeIn(100);
-
   const handleSignup = async () => {
-    setError({ email: '', password: '', reenter: '', login: '' });
+    setError({ email: '', password: '', reenter: '', signup: '' });
     let hasError = false;
+
+    // Email check
     if (!fields.email.trim()) {
       setError(prev => ({ ...prev, email: 'Please enter your email' }));
       hasError = true;
@@ -39,6 +33,8 @@ export default function SignUp() {
       setError(prev => ({ ...prev, email: 'Please enter valid email format' }));
       hasError = true;
     }
+
+    // Password check
     if (!fields.password.trim()) {
       setError(prev => ({ ...prev, password: 'Please enter your password' }));
       hasError = true;
@@ -49,13 +45,15 @@ export default function SignUp() {
         hasError = true;
       }
     }
+
+    // Re-enter password check
     if (!fields.reenter.trim()) {
       setError(prev => ({ ...prev, reenter: 'Please re-enter your password' }));
       hasError = true;
     } else if (fields.reenter !== fields.password) {
       setError(prev => ({
         ...prev,
-        reenter: 'Please re-enter the same password',
+        reenter: 'Passwords do not match',
       }));
       hasError = true;
     }
@@ -64,34 +62,24 @@ export default function SignUp() {
 
     try {
       setLoading(true);
-      console.log('sign up');
-      // Check if user exists
+      // Check if user already exists
       const exists = await checkUserExists(fields.email.trim().toLowerCase());
       if (exists) {
-        setAlert({
-          visible: true,
-          title: 'User exists',
-          message: 'An account with this email already exists!',
-        });
+        setError(prev => ({
+          ...prev,
+          signup: 'An account with this email already exists',
+        }));
         return;
       }
-      // Supabase sign up
+      // Sign up
       await userSignup(fields.email.trim().toLowerCase(), fields.password);
-      setAlert({
-        visible: true,
-        title: 'Signup successful',
-        message: 'Please check your email to verify your account!',
-      });
+      router.replace('/(auth)/login');
     } catch (error: any) {
-      const message =
-        error?.message || error?.error_description || 'Unknown error occurred';
-
-      console.log('signup error: ', error);
-      setAlert({
-        visible: true,
-        title: 'Signup failed',
-        message,
-      });
+      console.log('Signup failed: ', error);
+      setError(prev => ({
+        ...prev,
+        signup: error?.message || 'Unknown error occurred',
+      }));
     } finally {
       setLoading(false);
     }
@@ -99,7 +87,7 @@ export default function SignUp() {
 
   return (
     <ThemedSafeAreaView className="flex-1">
-      <View className="flex-1 justify-center px-4" style={fadeIn}>
+      <View className="flex-1 justify-center px-4">
         <View className="mb-8 items-center gap-1">
           <Text className="font-heading text-[50px] text-secondary-500">
             HealthSync
@@ -107,11 +95,14 @@ export default function SignUp() {
           <Text className="font-heading text-head2 text-primary-500">
             Sign Up
           </Text>
-          <Text className="text-primary-200">Create a new account</Text>
+          <Text className="text-primary-200">
+            Create a new account via email
+          </Text>
         </View>
-        
+
         <View className="gap-4">
           <View className="gap-3">
+            {/* Email */}
             <View className="gap-1">
               <TextInput
                 placeholder="Enter your email"
@@ -122,6 +113,8 @@ export default function SignUp() {
                 <Text className="text-error-500">{error.email}</Text>
               )}
             </View>
+
+            {/* Password */}
             <View className="gap-1">
               <PasswordInput
                 onChangeText={text => setFields({ ...fields, password: text })}
@@ -130,23 +123,28 @@ export default function SignUp() {
                 <Text className="text-error-500">{error.password}</Text>
               )}
             </View>
+
+            {/* Re-enter Password */}
             <View className="gap-1">
               <PasswordInput
-                onChangeText={text => setFields({ ...fields, reenter: text })}
                 placeholder="Re-enter your password"
+                onChangeText={text => setFields({ ...fields, reenter: text })}
               />
               {error.reenter !== '' && (
                 <Text className="text-error-500">{error.reenter}</Text>
               )}
             </View>
           </View>
+
+          {/* Sign Up Button */}
           <AnimatedPressable onPress={handleSignup} className="button">
             <Text className="text-xl font-bodyBold text-background-500">
-              {loading ? 'Creating new account...' : 'Sign Up'}
+              {loading ? 'Creating account...' : 'Sign Up'}
             </Text>
           </AnimatedPressable>
         </View>
 
+        {/* Social Signup */}
         <View className="my-6 flex-row items-center">
           <View className="h-[1px] flex-1 bg-primary-300" />
           <Text className="mx-3 text-primary-300">
@@ -164,23 +162,22 @@ export default function SignUp() {
             </Text>
           </AnimatedPressable>
         </View>
-        
+
+        {/* Already have account */}
         <View className="mt-4 flex-row justify-center">
-          <Text className="text-primary-500">Have an account? </Text>
+          <Text className="text-primary-500">Already have an account? </Text>
           <AnimatedPressable onPress={() => router.replace('/(auth)/login')}>
             <Text className="font-bodyBold text-secondary-500">Log In</Text>
           </AnimatedPressable>
         </View>
-      </View>
 
-      <CustomAlert
-        visible={alert.visible}
-        title={alert.title}
-        message={alert.message}
-        onConfirm={() => {
-          setAlert({ ...alert, visible: false });
-        }}
-      />
+        {/* Signup Error */}
+        {error.signup !== '' && (
+          <View className="mt-4 items-center">
+            <Text className="text-error-500">{error.signup}</Text>
+          </View>
+        )}
+      </View>
     </ThemedSafeAreaView>
   );
 }
