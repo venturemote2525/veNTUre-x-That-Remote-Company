8a50ac9983f4f980fc534e50a1036d1d07c1272d
xin wei <roylim0218@gmail.com>
2025-09-25 16:34:15 +0800

    linking backend to frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,25 @@
 
 # dummy data for now
 from fastapi import FastAPI
-from .db.supabase_client import supabase  
+from fastapi.middleware.cors import CORSMiddleware
+from .db.supabase_client import supabase
 from .routes import food, auth
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["exp://192.168.1.237:8081"],  # In production, specify your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Include routers
 app.include_router(food.router, prefix="/food", tags=["food"])
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(food.router, prefix="/inference", tags=["Inference"])
 
 @app.get("/")
 def root():
